ae6274134ebf6577202770173c249c8e98e05f73
Kajal C <kajalchakole@yahoo.co.in>
2019-01-04 22:47:18 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`;
+  fs.appendFile('server.log', `${log}`, () => {});
+  res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!!</h1>');
+  res.render('home.hbs', {
+    welcomeMessage: "Welcome to my site",
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  // res.send({
+  //   likes: 'Music',
+  //   name: 'Kajal Chakole'
+  // });
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up.');
+});
